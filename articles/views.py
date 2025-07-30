70a7ae302ed07f8b59b02a92309cdf54ac95800a
rarciniegas <rrarciniegas@hotmail.com>
2025-07-29 22:24:41 -0400

    permissions and authorizations

--- articles/views.py
+++ articles/views.py
@@ -1,30 +1,45 @@
 # articles/views.py
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'  # Specify your template name here
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'  # Specify your template name here        
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ("title", "body")  # Specify the fields you want to edit
     template_name = 'article_edit.html'  # Specify your template name here
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'  # Specify your template name here
     success_url = reverse_lazy('article_list')  # Redirect to the article list after deletion
 
-class ArticleCreateView(CreateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
-    fields = ("title", "body", "author")  # Specify the fields you want to create
+    fields = ("title", "body")  # Specify the fields you want to create
     template_name = 'article_new.html'  # Specify your template name here
-    success_url = reverse_lazy('article_list')  # Redirect to the article list after creation
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
