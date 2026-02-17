2a8f4d754cf02e570fffa9e1a9fa6665605943e1
rarciniegas <rrarciniegas@hotmail.com>
2026-02-16 21:04:40 -0500

    custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    # Add any additional fields you want for your custom user model
+    age = models.PositiveIntegerField(null=True, blank=True)
